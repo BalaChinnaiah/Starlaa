674a109fc639959e66eaf16718be61e71bf7ed4f
BalaChinnaiah <balasagar548@gmail.com>
2024-11-13 19:40:47 +0530

    FInal Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/sidebar';
+import Main from './components/Main/Main';
+
+
+
+const HelloBala = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default HelloBala;
